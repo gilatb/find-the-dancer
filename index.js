7c13e83899bfb4305b54dccc66fff705224e6542
Gio Grand <gio.grand@me.com>
2019-10-10 20:24:19 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const { dancersPool, participants } = require('./test')
 
 const findTheDancer = (participants, dancersPool) => {
-  // Your code here:  
+  // Your code here: 
+  
+  let okCouples = []
+  for (let i of participants) {
+    for (let y of participants) {
+      if (i.style == y.style && i !== y) {
+        okCouples.push(i.style)
+      }
+    }
+  } 
+  let notOkCouples = []
+  let notOkDancer
+  for (let x of participants) {
+    if (!okCouples.includes(x.style)) {
+      notOkCouples.push(x.style)
+      notOkDancer = x.name
+    }
+  }
+
+  let potential = dancersPool[notOkCouples[0]]
+
+  if (notOkDancer.toUpperCase() == notOkDancer) {
+    for (let v = 0; v < potential.length; v++) {
+      if(potential[v].toUpperCase() != potential[v]) {
+        return potential[v]
+      }
+    }
+  }
+
 
 }
 
